2ab190838eeba798c168e3abe3b98614165d4451
Marco Perone <pasafama@gmail.com>
2020-04-24 15:28:46 +0200

    rename to `compareInclusive` and `compareExclusive`

--- test/AvailerSpec/BoundarySpec.hs
+++ test/AvailerSpec/BoundarySpec.hs
@@ -60,42 +60,42 @@ spec =
     it "compares an excluded and an included equal ending boundary returning LT" $ property $
       \(a1 :: Int) -> compareEnd (Boundary a1 Excluded) (Boundary a1 Included) == LT
 
-    -- compareStartEnd
+    -- compareInclusive
 
     it "compares a starting and an ending included boundaries looking at their values" $ property $
-      \(a1 :: Int) a2 -> compareStartEnd (Boundary a1 Included) (Boundary a2 Included) == compare a1 a2
+      \(a1 :: Int) a2 -> compareInclusive (Boundary a1 Included) (Boundary a2 Included) == compare a1 a2
 
     it "compares a starting and an ending excluded boundaries looking at their values" $ property $
-      \(a1 :: Int) a2 -> compareStartEnd (Boundary a1 Excluded) (Boundary a2 Excluded) == compare a1 a2 <> GT
+      \(a1 :: Int) a2 -> compareInclusive (Boundary a1 Excluded) (Boundary a2 Excluded) == compare a1 a2 <> GT
 
     it "compares an included starting and an excluded ending boundary looking at their values and returning GT if they are equal" $ property $
-      \(a1 :: Int) a2 -> compareStartEnd (Boundary a1 Included) (Boundary a2 Excluded) == compare a1 a2 <> GT
+      \(a1 :: Int) a2 -> compareInclusive (Boundary a1 Included) (Boundary a2 Excluded) == compare a1 a2 <> GT
 
     it "compares an included starting and an excluded ending boundary with equal values returning GT" $ property $
-      \(a1 :: Int) -> compareStartEnd (Boundary a1 Included) (Boundary a1 Excluded) == GT
+      \(a1 :: Int) -> compareInclusive (Boundary a1 Included) (Boundary a1 Excluded) == GT
 
     it "compares an excluded starting and an included ending boundary looking at their values and returning GT if they are equal" $ property $
-      \(a1 :: Int) a2 -> compareStartEnd (Boundary a1 Excluded) (Boundary a2 Included) == compare a1 a2 <> GT
+      \(a1 :: Int) a2 -> compareInclusive (Boundary a1 Excluded) (Boundary a2 Included) == compare a1 a2 <> GT
 
     it "compares an excluded starting and an included ending boundary with equal values returning GT" $ property $
-      \(a1 :: Int) -> compareStartEnd (Boundary a1 Excluded) (Boundary a1 Included) == GT
+      \(a1 :: Int) -> compareInclusive (Boundary a1 Excluded) (Boundary a1 Included) == GT
 
-    -- compareEndStart
+    -- compareExclusive
 
     it "compares an ending and a starting included boundaries looking at their values" $ property $
-      \(a1 :: Int) a2 -> compareEndStart (Boundary a1 Included) (Boundary a2 Included) == compare a1 a2
+      \(a1 :: Int) a2 -> compareExclusive (Boundary a1 Included) (Boundary a2 Included) == compare a1 a2
 
     it "compares an ending and a starting excluded boundaries looking at their values" $ property $
-      \(a1 :: Int) a2 -> compareEndStart (Boundary a1 Excluded) (Boundary a2 Excluded) == compare a1 a2 <> LT
+      \(a1 :: Int) a2 -> compareExclusive (Boundary a1 Excluded) (Boundary a2 Excluded) == compare a1 a2 <> LT
 
     it "compares an included ending and an excluded starting boundary looking at their values and returning LT if they are equal" $ property $
-      \(a1 :: Int) a2 -> compareEndStart (Boundary a1 Included) (Boundary a2 Excluded) == compare a1 a2 <> LT
+      \(a1 :: Int) a2 -> compareExclusive (Boundary a1 Included) (Boundary a2 Excluded) == compare a1 a2 <> LT
 
     it "compares an included ending and an excluded starting boundary with equal values returning LT" $ property $
-      \(a1 :: Int) -> compareEndStart (Boundary a1 Included) (Boundary a1 Excluded) == LT
+      \(a1 :: Int) -> compareExclusive (Boundary a1 Included) (Boundary a1 Excluded) == LT
 
     it "compares an excluded ending and an included starting boundary looking at their values and returning LT if they are equal" $ property $
-      \(a1 :: Int) a2 -> compareEndStart (Boundary a1 Excluded) (Boundary a2 Included) == compare a1 a2 <> LT
+      \(a1 :: Int) a2 -> compareExclusive (Boundary a1 Excluded) (Boundary a2 Included) == compare a1 a2 <> LT
 
     it "compares an excluded ending and an included starting boundary with equal values returning LT" $ property $
-      \(a1 :: Int) -> compareEndStart (Boundary a1 Excluded) (Boundary a1 Included) == LT
+      \(a1 :: Int) -> compareExclusive (Boundary a1 Excluded) (Boundary a1 Included) == LT
