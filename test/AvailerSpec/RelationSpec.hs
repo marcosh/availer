38de2450db7610bb1218aa49a4d19db2de0fc655
Marco Perone <pasafama@gmail.com>
2020-04-29 18:56:23 +0200

    start testing relate properties

--- test/AvailerSpec/RelationSpec.hs
+++ test/AvailerSpec/RelationSpec.hs
@@ -10,15 +10,26 @@ import Test.QuickCheck
 
 import Availer.Interval
 import Availer.Relation
-import AvailerSpec.Arbitrary.Interval()
+import AvailerSpec.Arbitrary.Interval
 import AvailerSpec.Arbitrary.Relation()
 
 spec :: Spec
 spec =
-  describe "invert" $ do
+  describe "Relation" $ do
 
-    it "is an involution" $ property $
-      \relation -> invert (invert relation) == relation
+    describe "relate" $ do
 
-    it "inverts relations" $ property $
-      \(interval1 :: Interval Int) interval2 -> relate interval1 interval2 == invert (relate interval2 interval1)
+      it "detects equal intervals" $ property $
+        \(interval :: Interval Int) -> relate interval interval == Equal
+
+      it "detects that Empty is contained in any non-empty interval" $ property $
+        forAll genNonEmptyInterval $
+          \(interval :: Interval Int) -> relate interval empty == Contains
+
+    describe "invert" $ do
+
+      it "is an involution" $ property $
+        \relation -> invert (invert relation) == relation
+
+      it "inverts relations" $ property $
+        \(interval1 :: Interval Int) interval2 -> relate interval1 interval2 == invert (relate interval2 interval1)
